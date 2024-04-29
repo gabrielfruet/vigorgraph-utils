d0911ef2c720f9446ebcdaa63cf608b8ba556ca7
gabrielfruet <gabrielfruet538@gmail.com>
2024-04-29 15:06:37 -0300

    ...

--- src/vectorization.py
+++ src/vectorization.py
@@ -1,13 +1,16 @@
 import logging
 import atexit
-from pprint import pprint
 import cv2 as cv
+import json
+from pprint import pprint
+from typing import Tuple, List
 from algorithms import edge_linking, rdp
 from preprocessing import split_image, apply_skeletonize, find_background_color_range, find_background_mask
 import numpy as np
 
 atexit.register(cv.destroyAllWindows)
 
+SHOW_IMAGE = True
 GD_IMG_PATH = "/home/gabrielfruet/dev/python/vigorgraph/dataset/plantulas_soja/1/ground_truth/C2T1R1.jpg"
 INPUT_IMG_PATH = "/home/gabrielfruet/dev/python/vigorgraph/dataset/plantulas_soja/1/input/C2T1R1.jpg"
 
@@ -23,27 +26,49 @@ def draw_lines(img, pts, color):
         cv.line(img, pts[i], pts[i+1], color=color)
 
     for i in range(n):
+        cv.circle(img, pts[i], 2, (255,255,255), 2)
         j,i = pts[i]
         img[i,j] = (255,255,255)
 
-SHOW_IMAGE = True
+def find_lines(gd_img: np.ndarray, epsilon=5) -> Tuple[List[np.ndarray], List[np.ndarray]]:
+    """
+
+    Returns
+
+    - tuple of numpy.ndarray
+        1st is the Raiz Primaria links
+        2nd is the Hipocotilo links
+    """
+    raiz_prim, hipocotilo = split_image(gd_img)
+    raiz_prim = apply_skeletonize(raiz_prim)
+    hipocotilo = apply_skeletonize(hipocotilo)
+    raiz_prim_links = edge_linking(raiz_prim)
+    hipocotilo_links = edge_linking(hipocotilo)
+    raiz_prim_links_rdp = [rdp(link,epsilon=epsilon) for link in raiz_prim_links]
+    hipocotilo_links_rdp = [rdp(link,epsilon=epsilon) for link in hipocotilo_links]
+    return raiz_prim_links_rdp, hipocotilo_links_rdp
+
+def rm_bg(img: np.ndarray):
+    color_range = find_background_color_range(img)
+    color_range = find_background_color_range(img)
+    input_img_wo_background = find_background_mask(img, color_range)
+    return input_img_wo_background
+
+def find_seed_blobs(input_img_wo_background: np.ndarray):
+    input_img_wo_background = cv.erode(input_img_wo_background, np.ones((15,15)))
+    contours, _ = cv.findContours(input_img_wo_background, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    input_img_wo_background = cv.cvtColor(input_img_wo_background, cv.COLOR_GRAY2BGR)
+    return contours
 
 def run():
     logging.getLogger().setLevel(logging.WARNING)
     gd_img = cv.imread(GD_IMG_PATH)
     input_img = cv.imread(INPUT_IMG_PATH)
 
-    raiz_prim, hipocotilo = split_image(gd_img)
-    raiz_prim = apply_skeletonize(raiz_prim)
-    hipocotilo = apply_skeletonize(hipocotilo)
-    raiz_prim_links = edge_linking(raiz_prim)
-    hipocotilo_links = edge_linking(hipocotilo)
+    raiz_prim_links_rdp, hipocotilo_links_rdp = find_lines(gd_img, epsilon=40)
 
-    linked_raiz_prim = cv.cvtColor(np.zeros_like(raiz_prim), cv.COLOR_GRAY2BGR)
-    linked_hipocotilo = cv.cvtColor(np.zeros_like(hipocotilo), cv.COLOR_GRAY2BGR)
-
-    raiz_prim_links_rdp = [rdp(link,epsilon=5) for link in raiz_prim_links]
-    hipocotilo_links_rdp = [rdp(link,epsilon=5) for link in hipocotilo_links]
+    linked_raiz_prim = np.zeros_like(gd_img)
+    linked_hipocotilo = np.zeros_like(gd_img)
     
     for rdplink in raiz_prim_links_rdp:
         draw_lines(linked_raiz_prim, rdplink, color=(0,255,0))
@@ -51,31 +76,25 @@ def run():
     for rdplink in hipocotilo_links_rdp:
         draw_lines(linked_hipocotilo, rdplink, color=(0,0,255))
 
-    color_range = find_background_color_range(input_img)
-
-    input_img_wo_background = find_background_mask(input_img, color_range)
-    input_img_wo_background = cv.erode(input_img_wo_background, np.ones((15,15)))
-    contours, hierarchy = cv.findContours(input_img_wo_background, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-
-    input_img_wo_background = cv.cvtColor(input_img_wo_background, cv.COLOR_GRAY2BGR)
+    input_img_wo_background = rm_bg(input_img)
+    contours = find_seed_blobs(input_img_wo_background)
 
     cv.drawContours(input_img_wo_background, contours, -1, (255,0,255), 0);
 
-    output_json = {
+    output = {
         'Links': {
             'Hipocotilo': [link.tolist() for link in hipocotilo_links_rdp],
             'Raiz Primaria': [link.tolist() for link in raiz_prim_links_rdp]
         }
     }
-    pprint(output_json)
+    json_output = json.dumps(output)
+    pprint(json_output)
 
     if SHOW_IMAGE:
         while True:
-            cv.imshow('raiz_prim', raiz_prim)
-            cv.imshow('hip', hipocotilo)
             cv.imshow('linked_raiz_prim', linked_raiz_prim)
             cv.imshow('linked_hipocotilo', linked_hipocotilo)
-            cv.imshow('input', input_img)
+            cv.imshow('seed blobs', input_img_wo_background)
 
             key = cv.waitKey(1) & 0xFF
 
